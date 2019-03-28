4cf14987f5b53b9dc9ba725ad268586638b34975
q511352394 <46996219+q511352394@users.noreply.github.com>
2019-03-28 10:35:40 +0800

    Create SpringBoot_Run.java
    springboot A main startup class

---
+++ SpringBoot_Run.java
@@ -0,0 +1,17 @@
+package com.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//排除数据源的加载项
+@SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
+@EnableConfigServer		//开启配置中心设置
+public class SpringBoot_Run {
+
+	//定义主启动类  consumer8020启动类
+	public static void main(String[] args) {
+		//启动SpringBoot程序
+		//DubboBannerApplicationListener. //关闭spring图
+		SpringApplication.run(SpringBoot_Run.class, args);
+	}
+}
